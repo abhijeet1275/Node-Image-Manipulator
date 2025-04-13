4903ed9088d83ec414946297654a80219b6fcb7d
Abhijeet Kumar <142255236+abhijeet1275@users.noreply.github.com>
2025-04-14 04:40:41 +0530

    Update node.h

--- node.h
+++ node.h
@@ -3,11 +3,19 @@
 
 #include <QImage>
 #include <QPoint>
+#include <QString>
+#include <QList>
+#include <QMap>
+#include <QVariant>
+#include "node_property.h"
+
 class Node
 {
 public:
-    Node(const QImage &image, const QPoint &position);
+    // Constructor
+    Node(const QImage &image, const QPoint &position, const QString &type = "default", const QString &name = "");
 
+    // Getter/Setter for node properties
     QImage getImage() const;
     QPoint getPosition() const;
     void setPosition(const QPoint &position);
@@ -18,11 +26,36 @@ public:
     bool isSelected() const;
     void setSelected(bool selected);
 
+    QString getType() const;
+    void setType(const QString &type);
+
+    QString getName() const;
+    void setName(const QString &name);
+
+    // Node properties management
+    void addProperty(const QString &name, const QVariant &value, NodeProperty::Type type);
+    NodeProperty *getProperty(const QString &name);
+    QList<NodeProperty *> getAllProperties() const;
+    bool hasProperty(const QString &name) const;
+
+    // Create default properties based on node type
+    void initializeDefaultProperties();
+
+    // Children management
+    void addChildNode(Node *child);
+    // Add this to the public section of Node class in node.h
+    void removeChildNode(Node *child);
+    QList<Node *> getChildren() const; // ✅ Added this line
+
 private:
     QImage m_image;
     QPoint m_position;
     bool m_dragging;
-    bool m_selected = false;
+    bool m_selected;
+    QString m_type;
+    QString m_name;
+    QList<Node *> m_children;                   // List of child nodes
+    QMap<QString, NodeProperty *> m_properties; // Properties for this node
 };
 
 #endif // NODE_H
