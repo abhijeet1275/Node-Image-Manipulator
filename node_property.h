02f2dd7f9fa37253efead5d6acc538ba73e273ad
Abhijeet Kumar <142255236+abhijeet1275@users.noreply.github.com>
2025-04-14 04:44:31 +0530

    Update and rename NodeManager.cpp 20-31-48-572.cpp to node_property.h

---
+++ node_property.h
@@ -0,0 +1,42 @@
+#ifndef NODE_PROPERTY_H
+#define NODE_PROPERTY_H
+
+#include <QString>
+#include <QVariant>
+
+class NodeProperty
+{
+public:
+    enum Type
+    {
+        Blur_Radius,
+        Image_contrast,
+        Integer,
+        Double,
+        String,
+        Boolean,
+        Enum,
+        CustomList
+    };
+
+    // Constructor
+    NodeProperty(const QString &name, const QVariant &value, Type type)
+        : m_name(name), m_value(value), m_type(type) {}
+
+    // Getter/Setter for property
+    QString getName() const { return m_name; }
+    QVariant getValue() const { return m_value; }
+    void setValue(const QVariant &value) { m_value = value; }
+    Type getType() const { return m_type; }
+    // For enum types
+    void setEnumValues(const QStringList &values) { m_enumValues = values; }
+    QStringList getEnumValues() const { return m_enumValues; }
+private:
+    QString m_name;
+    QVariant m_value;
+    Type m_type;
+    QStringList m_enumValues; // For enum types
+    // List of child nodes
+};
+
+#endif // NODE_PROPERTY_H
