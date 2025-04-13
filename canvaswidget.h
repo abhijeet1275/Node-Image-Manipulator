0fde5463ed8e14aa48bc8bbe9b661b8be50431e7
Abhijeet Kumar <142255236+abhijeet1275@users.noreply.github.com>
2025-04-14 04:43:33 +0530

    Update canvaswidget.h

--- canvaswidget.h
+++ canvaswidget.h
@@ -1,3 +1,4 @@
+// canvaswidget.h
 #ifndef CANVASWIDGET_H
 #define CANVASWIDGET_H
 
@@ -13,19 +14,27 @@ class CanvasWidget : public QWidget
     Q_OBJECT
 public:
     explicit CanvasWidget(QWidget *parent = nullptr);
-    void loadImage(const QImage &image);
+    void loadImage(const QImage &image, const QString &filePath = "");
+    void createNode(const QString &nodeType, const QString &nodeName = "");
+    const QList<Node> &getNodes() const { return m_nodes; }
+    Node *getSelectedNode();
+    QList<Node *> getAllNodes();
+signals:
+    void nodeSelected(Node *node);
+    void nodePropertyChanged(Node *node, const QString &propertyName);
 
 protected:
     void paintEvent(QPaintEvent *event) override;
     void mousePressEvent(QMouseEvent *event) override;
     void mouseMoveEvent(QMouseEvent *event) override;
     void mouseReleaseEvent(QMouseEvent *event) override;
+    
 
 private:
-    QImage m_image;
     QList<Node> m_nodes;
-    Node* m_draggedNode = nullptr;  // Currently dragged node
-    QPoint m_offset;  // Offset for the mouse inside the node
+    Node *m_draggedNode = nullptr; // Currently dragged node
+    QPoint m_offset;               // Offset for the mouse inside the node
+    int m_nodeCounter = 0;         // For generating unique node names
 };
 
 #endif // CANVASWIDGET_H
