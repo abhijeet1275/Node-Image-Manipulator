83b03952f2a25c406d7747af3d15a76c0aa3bc76
Abhijeet Kumar <142255236+abhijeet1275@users.noreply.github.com>
2025-04-14 04:41:23 +0530

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,12 +1,14 @@
+// mainwindow.h
 #pragma once
-
+#include <QLineEdit>
+#include <QCheckBox>
+#include <QVBoxLayout> // for addLayout, addStretch etc.
+#include <QHBoxLayout>
 #include <QMainWindow>
 #include <QString>
-
-struct BlurSettings {
-    int radius = 5;
-    QString blurType = "Uniform";
-};
+#include <QWidget>
+#include <QMap>
+#include "node.h"
 
 class MainWindow : public QMainWindow
 {
@@ -16,6 +18,17 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void handleNodeSelected(Node *node);
+    void updateNodeProperty(const QString &propertyName, const QVariant &value);
+    void handleDeleteChild();
+
 private:
-    BlurSettings blurSettings; // <=== Now you can use this in your logic
+    void setupNodeList();
+    void setupAdjustmentPanel(Node *node);
+    void clearAdjustmentPanel();
+    void updateCombinedGroupProperties(int value);
+
+        QWidget *m_adjustmentsPanel = nullptr;
+    Node *m_selectedNode = nullptr;
 };
